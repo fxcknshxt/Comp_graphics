c4e20e484e1f3d644f3fbfa6fa4f2e66c205d038
Alexey Nafikov <alexeysg2@gmail.com>
2025-12-26 09:44:01 +0500

    lab6

---
+++ lab6/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace OpenGL_GLControl
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.GroupBox groupBoxControls;
+        private System.Windows.Forms.GroupBox groupBoxDrawing;
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonRotate;
+        private System.Windows.Forms.Button buttonAngleZero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.groupBoxControls = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAngleZero = new System.Windows.Forms.Button();
+            this.buttonRotate = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBoxDrawing = new System.Windows.Forms.GroupBox();
+            this.glControl1 = new OpenTK.GLControl();
+            this.groupBoxControls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBoxDrawing.SuspendLayout();
+            this.SuspendLayout();
+
+            // groupBoxControls
+            this.groupBoxControls.Controls.Add(this.label2);
+            this.groupBoxControls.Controls.Add(this.label1);
+            this.groupBoxControls.Controls.Add(this.buttonAngleZero);
+            this.groupBoxControls.Controls.Add(this.buttonRotate);
+            this.groupBoxControls.Controls.Add(this.numericUpDown1);
+            this.groupBoxControls.Controls.Add(this.comboBox1);
+            this.groupBoxControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBoxControls.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxControls.Name = "groupBoxControls";
+            this.groupBoxControls.Size = new System.Drawing.Size(800, 100);
+            this.groupBoxControls.TabIndex = 0;
+            this.groupBoxControls.TabStop = false;
+            this.groupBoxControls.Text = "Управление";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Угол вращения:";
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ось вращ.:";
+
+            // buttonAngleZero
+            this.buttonAngleZero.Location = new System.Drawing.Point(450, 25);
+            this.buttonAngleZero.Name = "buttonAngleZero";
+            this.buttonAngleZero.Size = new System.Drawing.Size(100, 30);
+            this.buttonAngleZero.TabIndex = 3;
+            this.buttonAngleZero.Text = "Angle = 0";
+            this.buttonAngleZero.UseVisualStyleBackColor = true;
+            this.buttonAngleZero.Click += new System.EventHandler(this.buttonAngleZero_Click);
+
+            // buttonRotate
+            this.buttonRotate.Location = new System.Drawing.Point(330, 25);
+            this.buttonRotate.Name = "buttonRotate";
+            this.buttonRotate.Size = new System.Drawing.Size(100, 30);
+            this.buttonRotate.TabIndex = 2;
+            this.buttonRotate.Text = "Rotate";
+            this.buttonRotate.UseVisualStyleBackColor = true;
+            this.buttonRotate.Click += new System.EventHandler(this.buttonRotate_Click);
+
+            // numericUpDown1
+            this.numericUpDown1.Location = new System.Drawing.Point(200, 50);
+            this.numericUpDown1.Maximum = new decimal(new int[] { 360, 0, 0, 0 });
+            this.numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
+
+            // comboBox1
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] { "X", "Y", "Z" });
+            this.comboBox1.Location = new System.Drawing.Point(20, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 21);
+            this.comboBox1.TabIndex = 0;
+
+            // groupBoxDrawing
+            this.groupBoxDrawing.Controls.Add(this.glControl1);
+            this.groupBoxDrawing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxDrawing.Location = new System.Drawing.Point(0, 100);
+            this.groupBoxDrawing.Name = "groupBoxDrawing";
+            this.groupBoxDrawing.Size = new System.Drawing.Size(800, 500);
+            this.groupBoxDrawing.TabIndex = 1;
+            this.groupBoxDrawing.TabStop = false;
+            this.groupBoxDrawing.Text = "Область рисования";
+
+            // glControl1
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl1.Location = new System.Drawing.Point(3, 16);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(794, 481);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.groupBoxDrawing);
+            this.Controls.Add(this.groupBoxControls);
+            this.Name = "Form1";
+            this.Text = "OpenGL GLControl - Лаба 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxControls.ResumeLayout(false);
+            this.groupBoxControls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBoxDrawing.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ lab6/Form1.cs
@@ -0,0 +1,286 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OpenGL_GLControl
+{
+    public partial class Form1 : Form
+    {
+        bool loaded = false;
+        double angle = 0;
+        double dangle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            SetupViewport(glControl1);    // установка вида
+            comboBox1.SelectedIndex = 0;  // выбор оси для вращения (X)
+            numericUpDown1.Value = 3;     // начальный угол вращения
+        }
+
+        private void SetupViewport(GLControl glControl)
+        {
+            float aspectRatio = (float)glControl.Width / (float)glControl.Height;
+            GL.Viewport(0, 0, glControl.Width, glControl.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver4,    // 45 градусов
+                aspectRatio,
+                1f,
+                5000000000);
+            GL.MultMatrix(ref perspective);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded)
+                return;
+
+            // Очистка экрана
+            GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f); // цвет фона
+            GL.Enable(EnableCap.DepthTest);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // Настройка камеры
+            Matrix4 modelview = Matrix4.LookAt(
+                new Vector3(-300, 300, 200),  // позиция камеры
+                new Vector3(0, 0, 0),         // точка взгляда
+                new Vector3(0, 0, 1));        // направление "вверх"
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+
+            // Вращение вокруг выбранной оси
+            switch (comboBox1.Text)
+            {
+                case "X": GL.Rotate(angle, 1, 0, 0); break;
+                case "Y": GL.Rotate(angle, 0, 1, 0); break;
+                case "Z": GL.Rotate(angle, 0, 0, 1); break;
+            }
+
+            Vector3 clr1 = new Vector3(1.0f, 1.0f, 0.0f); // желтый
+            Vector3 clr2 = new Vector3(1.0f, 0.0f, 0.0f); // красный
+            Vector3 clr3 = new Vector3(0.2f, 0.9f, 1.0f); // голубой
+
+            // ===== РИСУЕМ ОСИ =====
+
+            // Ось X (желтая)
+            GL.Begin(BeginMode.Lines);
+            GL.Color3(clr1);
+            GL.Vertex3(-300.0f, 0.0f, 0.0f);
+            GL.Vertex3(300.0f, 0.0f, 0.0f);
+            GL.End();
+
+            // Ось Y (красная)
+            GL.Begin(BeginMode.Lines);
+            GL.Color3(clr2);
+            GL.Vertex3(0.0f, -300.0f, 0.0f);
+            GL.Vertex3(0.0f, 300.0f, 0.0f);
+            GL.End();
+
+            // Ось Z (голубая)
+            GL.Begin(BeginMode.Lines);
+            GL.Color3(clr3);
+            GL.Vertex3(0.0f, 0.0f, -300f);
+            GL.Vertex3(0.0f, 0.0f, 300.0f);
+            GL.End();
+
+            // ===== РИСУЕМ 3D ФИГУРЫ =====
+
+            float z = 40f;
+
+            // 1. Треугольник (из инструкции)
+            GL.Begin(BeginMode.Triangles);
+            GL.Color3(clr1);
+            GL.Vertex3(-100.0f, -100.0f, z);
+            GL.Color3(clr2);
+            GL.Vertex3(100.0f, -100.0f, z);
+            GL.Color3(clr3);
+            GL.Vertex3(0.0f, 100.0f, z);
+            GL.End();
+
+            // 2. Куб (добавляем к заданию)
+            DrawCube(0, 0, 100, 50);
+
+            // 3. Пирамида
+            DrawPyramid(150, 0, 100, 40);
+
+            // 4. Сфера
+            DrawSphere(-150, 0, 100, 30);
+
+            glControl1.SwapBuffers();
+        }
+
+        // ===== ФУНКЦИИ РИСОВАНИЯ 3D ФИГУР =====
+
+        private void DrawCube(float x, float y, float z, float size)
+        {
+            float s = size / 2;
+
+            // Передняя грань
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(1.0f, 0.0f, 0.0f); // Красный
+            GL.Vertex3(x - s, y - s, z + s);
+            GL.Vertex3(x + s, y - s, z + s);
+            GL.Vertex3(x + s, y + s, z + s);
+            GL.Vertex3(x - s, y + s, z + s);
+            GL.End();
+
+            // Задняя грань
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(0.0f, 1.0f, 0.0f); // Зеленый
+            GL.Vertex3(x - s, y - s, z - s);
+            GL.Vertex3(x - s, y + s, z - s);
+            GL.Vertex3(x + s, y + s, z - s);
+            GL.Vertex3(x + s, y - s, z - s);
+            GL.End();
+
+            // Верхняя грань
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(0.0f, 0.0f, 1.0f); // Синий
+            GL.Vertex3(x - s, y + s, z - s);
+            GL.Vertex3(x - s, y + s, z + s);
+            GL.Vertex3(x + s, y + s, z + s);
+            GL.Vertex3(x + s, y + s, z - s);
+            GL.End();
+
+            // Нижняя грань
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(1.0f, 1.0f, 0.0f); // Желтый
+            GL.Vertex3(x - s, y - s, z - s);
+            GL.Vertex3(x + s, y - s, z - s);
+            GL.Vertex3(x + s, y - s, z + s);
+            GL.Vertex3(x - s, y - s, z + s);
+            GL.End();
+
+            // Левая грань
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(1.0f, 0.0f, 1.0f); // Фиолетовый
+            GL.Vertex3(x - s, y - s, z - s);
+            GL.Vertex3(x - s, y - s, z + s);
+            GL.Vertex3(x - s, y + s, z + s);
+            GL.Vertex3(x - s, y + s, z - s);
+            GL.End();
+
+            // Правая грань
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(0.0f, 1.0f, 1.0f); // Голубой
+            GL.Vertex3(x + s, y - s, z - s);
+            GL.Vertex3(x + s, y + s, z - s);
+            GL.Vertex3(x + s, y + s, z + s);
+            GL.Vertex3(x + s, y - s, z + s);
+            GL.End();
+        }
+
+        private void DrawPyramid(float x, float y, float z, float size)
+        {
+            float s = size;
+
+            // Основание (квадрат)
+            GL.Begin(BeginMode.Quads);
+            GL.Color3(0.8f, 0.4f, 0.0f); // Оранжевый
+            GL.Vertex3(x - s, y - s, z);
+            GL.Vertex3(x + s, y - s, z);
+            GL.Vertex3(x + s, y + s, z);
+            GL.Vertex3(x - s, y + s, z);
+            GL.End();
+
+            // Боковые грани (треугольники)
+            GL.Begin(BeginMode.Triangles);
+            GL.Color3(0.6f, 0.8f, 0.2f); // Салатовый
+
+            // Передняя грань
+            GL.Vertex3(x - s, y - s, z);
+            GL.Vertex3(x + s, y - s, z);
+            GL.Vertex3(x, y, z + s * 2);
+
+            // Правая грань
+            GL.Vertex3(x + s, y - s, z);
+            GL.Vertex3(x + s, y + s, z);
+            GL.Vertex3(x, y, z + s * 2);
+
+            // Задняя грань
+            GL.Vertex3(x + s, y + s, z);
+            GL.Vertex3(x - s, y + s, z);
+            GL.Vertex3(x, y, z + s * 2);
+
+            // Левая грань
+            GL.Vertex3(x - s, y + s, z);
+            GL.Vertex3(x - s, y - s, z);
+            GL.Vertex3(x, y, z + s * 2);
+            GL.End();
+        }
+
+        private void DrawSphere(float x, float y, float z, float radius)
+        {
+            int segments = 16;
+            int rings = 8;
+
+            GL.Color3(1.0f, 0.5f, 0.0f); // Оранжевый
+
+            for (int i = 0; i < rings; i++)
+            {
+                double lat0 = Math.PI * (-0.5 + (double)(i) / rings);
+                double z0 = Math.Sin(lat0);
+                double zr0 = Math.Cos(lat0);
+
+                double lat1 = Math.PI * (-0.5 + (double)(i + 1) / rings);
+                double z1 = Math.Sin(lat1);
+                double zr1 = Math.Cos(lat1);
+
+                GL.Begin(BeginMode.QuadStrip);
+                for (int j = 0; j <= segments; j++)
+                {
+                    double lng = 2 * Math.PI * (double)(j) / segments;
+                    double sinLng = Math.Sin(lng);
+                    double cosLng = Math.Cos(lng);
+
+                    GL.Vertex3(
+                        x + radius * cosLng * zr0,
+                        y + radius * sinLng * zr0,
+                        z + radius * z0);
+
+                    GL.Vertex3(
+                        x + radius * cosLng * zr1,
+                        y + radius * sinLng * zr1,
+                        z + radius * z1);
+                }
+                GL.End();
+            }
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded) return;
+            SetupViewport(glControl1);
+        }
+
+        // ===== ОБРАБОТЧИКИ КНОПОК =====
+
+        private void buttonRotate_Click(object sender, EventArgs e)
+        {
+            dangle = (double)numericUpDown1.Value;
+            angle += dangle;
+            glControl1.Invalidate();
+        }
+
+        private void buttonAngleZero_Click(object sender, EventArgs e)
+        {
+            angle = 0;
+            glControl1.Invalidate();
+        }
+    }
+}
